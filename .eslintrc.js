d42b3795f5131ba2133902d0adfdf8a0eed72ce5
arx-8 <swans.randomness@gmail.com>
2020-07-02 23:21:26 +0900

    chore: Install ESLint family & Fix errors

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  extends: [
+    "standard",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:jest/recommended",
+    /** @see https://github.com/prettier/eslint-config-prettier#installation */
+    "prettier/standard",
+    "prettier/react",
+    "plugin:prettier/recommended",
+    "react-app",
+  ],
+  parser: "babel-eslint",
+  plugins: ["only-error", "sort-keys-fix"],
+  rules: {
+    "import/no-default-export": "error",
+    "no-param-reassign": "error",
+    "no-var": "error",
+    "object-shorthand": "error",
+
+    // prettier
+    "prettier/prettier": [
+      "error",
+      {
+        semi: false,
+      },
+    ],
+
+    "react/jsx-boolean-value": "error",
+    // jsx pragma に @emotion/core#jsx を使っている場合、Short Syntax 使えないため
+    "react/jsx-fragments": ["error", "element"],
+    "react/jsx-sort-props": "error",
+    "react/no-access-state-in-setstate": "error",
+    "react/no-array-index-key": "error",
+    "react/no-did-mount-set-state": "error",
+    "react/no-unsafe": ["error", { checkAliases: true }],
+    "react/prefer-stateless-function": "error",
+    "react/prop-types": "off",
+    "react/self-closing-comp": [
+      "error",
+      {
+        // empty な html が必要なケースはほとんどないため
+        html: false,
+      },
+    ],
+    "react/void-dom-elements-no-children": "error",
+    "require-await": "error",
+    "sort-keys-fix/sort-keys-fix": "error",
+  },
+}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from 'src/logo.svg';
-import 'src/App.css';
+import "src/App.css"
 
-function App() {
+import React from "react"
+import logo from "src/logo.svg"
+
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="logo" className="App-logo" src={logo} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
